408424df723ba5950df115e7bfb435a5afde92d4
ahillahaffat <108282632+ahillahaffat@users.noreply.github.com>
2023-11-22 19:50:02 +0700

    Update build_push_image.sh

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Build Docker image
-docker build -t ghcr.io/ahillahaffat/order-service:latest .
+docker build -t ghcr.io/ahillahaffat/shipping-service:latest .
 
 # Log in to GitHub Container Registry
 echo $GH_PACKAGES_TOKEN | docker login ghcr.io -u ahillahaffat --password-stdin
 
 # Push Docker image to GitHub Container Registry
-docker push ghcr.io/ahillahaffat/order-service:latest
+docker push ghcr.io/ahillahaffat/shipping-service:latest
